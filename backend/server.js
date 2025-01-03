695ae2bce0edf7c325c3e58ada6040197fe56e81
Nishant Singh <nishantsvis@gmail.com>
2025-01-03 20:28:47 +0530

    minor fix in getNotes

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,7 @@ app.get('/api/getNotes', async (req, res) => {
         if (user) {
             const startIndex = Math.max(0, req.body.questionNumber - 1)
             const endIndex = Math.min(startIndex + req.body.count - 1, user.questions.length - 1)
+            res.send(user.questions.slice(startIndex, endIndex + 1))
             //if startIndex exceeds the last index, it returns an empty array
         }
         else {
