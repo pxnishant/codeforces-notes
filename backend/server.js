4f05a17f5dabf683bda75dad47919cfbe2644662
Nishant Singh <nishantsvis@gmail.com>
2025-01-03 20:51:33 +0530

    created deleteNote API

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,7 @@ app.post('/api/addNote', async (req, res) => {
     if (!qname || !url || !rating || !note) {
         console.log('Bad request')
         res.status(400).end('All headers not specified')
+        return
     }
     
     const newQuestion = {
@@ -89,6 +90,31 @@ app.post('/api/addNote', async (req, res) => {
         console.error("Couldn't add note / POST request failed: ", e)
         res.status(500).end() 
 
+    }
+
+})
+
+app.delete('/api/deleteNote', async (req, res) => {
+
+    try {
+
+        await User.updateOne(
+            { username: req.body.username },
+            { $unset: {[`questions.${req.body.questionNumber - 1}`]: 1} }
+        )
+
+        await User.updateOne(
+            { username: req.body.username },
+            { $pull: { questions: null } }
+        )
+
+        res.status(204).end("Successfully deleted")
+
+    }
+
+    catch (e) {
+        console.error('Could not delete question: ', e)
+        res.status(500).end()
     }
 
 })
