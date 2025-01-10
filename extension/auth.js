5c55667655a24ff74ce818e1f8d24533f91192ea
Nishant Singh <nishantsvis@gmail.com>
2025-01-10 16:23:42 +0530

    Minor changes.

---
+++ extension/auth.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button')
+const emailInput = document.getElementById('emailInput')
+button.addEventListener('click', async () => {
+
+    const email = emailInput.textContent
+
+})
