eb8b3fba142341c574175961d153b771951d69c9
Nishant Singh <nishantsvis@gmail.com>
2025-01-11 15:44:05 +0530

    Attempting to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const authRoutes = require('./routers/authRoutes')
 const notesRoutes = require('./routers/notesRoutes')
 require('dotenv').config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 //database initialization
 mongoose.connect(process.env.MONGODB_CONN_URL).then(() => {
@@ -20,7 +20,10 @@ app.use(cookieParser());
 //routes
 app.use('/api/auth', authRoutes)
 app.use('/api', notesRoutes)
+app.get("/", (req, res) => res.send("Running backend."));
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
+
+module.exports = app;
